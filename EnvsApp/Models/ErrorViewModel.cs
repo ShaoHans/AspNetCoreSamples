d822e22109a3e22b0bb2e93333b8beb6bcbc2f2e
邵汉中 <635260101@qq.com>
2018-01-23 17:06:28 +0800

    添加项目文件。

---
+++ EnvsApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EnvsApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
