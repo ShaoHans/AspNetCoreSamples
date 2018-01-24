ed56e186e735d5d450c9422fdef11a92df0ae34d
邵汉中 <635260101@qq.com>
2018-01-24 11:07:02 +0800

    Options操作

---
+++ OptionsApp/Controllers/SettingsController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using OptionsApp.Models;
+using Microsoft.Extensions.Options;
+
+namespace OptionsApp.Controllers
+{
+    public class SettingsController : Controller
+    {
+        private readonly AppSettings _appSettings;
+
+        public SettingsController(IOptionsSnapshot<AppSettings> optionsAccessor)
+        {
+            _appSettings = optionsAccessor.Value;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_appSettings);
+        }
+    }
+}
---
+++ OptionsApp/Models/AppSettings.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OptionsApp.Models
+{
+    public class AppSettings
+    {
+        public OAuthServer OAuthServers { get; set; }
+    }
+
+    public class OAuthServer
+    {
+        public ThirdPartyOAuth QQ { get; set; }
+
+        public ThirdPartyOAuth WeChat { get; set; }
+
+        public ThirdPartyOAuth Weibo { get; set; }
+    }
+
+    public class ThirdPartyOAuth
+    {
+        public string OAuthUrl { get; set; }
+
+        public string AppId { get; set; }
+
+        public string AppSecret { get; set; }
+
+    }
+}
