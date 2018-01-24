7f2fe4a01dafe27c989c6bd049584a2e12c2bce5
邵汉中 <635260101@qq.com>
2018-01-24 16:31:40 +0800

    Cache

---
+++ InMemoryApp/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using InMemoryApp.Models;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace InMemoryApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IMemoryCache _memoryCache;
+        private readonly string timeCacheKey = "Time_Cache";
+        public HomeController(IMemoryCache memoryCache)
+        {
+            _memoryCache = memoryCache;
+        }
+
+        public IActionResult Index()
+        {
+            if(!_memoryCache.TryGetValue(timeCacheKey, out DateTime cacheTime))
+            {
+                cacheTime = DateTime.Now;
+                var options = new MemoryCacheEntryOptions().SetSlidingExpiration(TimeSpan.FromSeconds(3));
+                _memoryCache.Set(timeCacheKey, cacheTime, options);
+            }
+            ViewBag.CacheTime = cacheTime;
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ RedisCacheApp/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RedisCacheApp.Models;
+using Microsoft.Extensions.Caching.Distributed;
+using System.Text;
+
+namespace RedisCacheApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IDistributedCache _cache;
+        private string TimeCacheKey = "Time_Cache";
+        public HomeController(IDistributedCache cache)
+        {
+            _cache = cache;
+        }
+
+        public IActionResult Index()
+        {
+            var cacheTime = _cache.Get(TimeCacheKey);
+            if(cacheTime == null)
+            {
+                cacheTime = Encoding.UTF8.GetBytes(DateTime.Now.ToString());
+                _cache.Set(TimeCacheKey, cacheTime);
+            }
+            ViewBag.CacheTime = Encoding.UTF8.GetString(cacheTime);
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
